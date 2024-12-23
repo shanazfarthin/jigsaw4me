9f1e429f1a5d25dc5cba247b001b148290593b6f
shanaz <shanazthaha@gmail.com>
2024-12-23 20:59:28 +0530

    Updates

---
+++ precache-manifest.9c599a6227013707114dfb2e9c96501c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5147f6723ec4c66783df6f80d75af07",
+    "url": "/jigsaw4me/index.html"
+  },
+  {
+    "revision": "b91ea1e89cf22d99ee53",
+    "url": "/jigsaw4me/static/css/main.b497d7ae.chunk.css"
+  },
+  {
+    "revision": "638e200e69d950262938",
+    "url": "/jigsaw4me/static/js/2.ae026ab9.chunk.js"
+  },
+  {
+    "revision": "b91ea1e89cf22d99ee53",
+    "url": "/jigsaw4me/static/js/main.44901e0e.chunk.js"
+  },
+  {
+    "revision": "3ac5d3b726391539e8b8",
+    "url": "/jigsaw4me/static/js/runtime~main.96901f77.js"
+  },
+  {
+    "revision": "e58a1e40adf37747efd2324dcc20d729",
+    "url": "/jigsaw4me/static/media/puzzle back.e58a1e40.png"
+  }
+]);
